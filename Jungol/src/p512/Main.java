d1fda395e078d60356214db967cc66a36c03add9
Administrator <Administrator@User-2022EKRFIX>
2022-11-02 17:32:45 +0900

    오늘 문제풀이

--- Jungol/src/p512/Main.java
+++ Jungol/src/p512/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		int num = 49;
 		double d = 0.2683;
-		double c = num*d;
-		
+		double c = num * d;
+
 		System.out.printf("%d * %f = %.6f", num, d, c);
 
 	}
