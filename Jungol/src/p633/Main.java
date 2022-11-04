4fb05e550d1397012da70fade93e724be2af4264
Administrator <Administrator@User-2022EKRFIX>
2022-11-04 18:01:56 +0900

    문제풀이

--- Jungol/src/p633/Main.java
+++ Jungol/src/p633/Main.java
@@ -6,10 +6,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i = sc.nextInt();
-		sc.close();
-		System.out.println(i);
-
-	}
-
+		
+		
+//		System.out.println(i);
+		int i;
+		 do {
+			 System.out.print("1. korea");
+			 System.out.print("1. korea");
+			 System.out.print("1. korea");
+			 System.out.print("1. korea");
+			 System.out.print("1. korea");
+			 int i = sc.nextInt();
+			 
+			 System.out.println();
+			 if(i == 1) {
+				 System.out.println("Seoul");
+			 }else if (i==2) {
+				 System.out.println("Seoul");
+			 }else if (i==2) {
+				 System.out.println("Seoul");
+			 }else if (i==2) {
+				 System.out.println("Seoul");
+			 }
+			 System.out.println();					
+		 }while(1<= i && i <= 4); 
+		 sc.close();	
+		 System.out.println("none");
+			}
+		}
+    }
 }
