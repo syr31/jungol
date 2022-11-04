edf813358cb399c14b257147f2f6280dcb96da40
Administrator <Administrator@User-2022EKRFIX>
2022-11-04 17:22:02 +0900

    538 반복제어문 문제풀이

---
+++ Jungol/src/p538/Main.java
@@ -0,0 +1,26 @@
+package p538;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("numder?");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+
+//		System.out.println(num);
+
+		for (;;) {
+			System.out.println("numder?");
+			if (0 < num) {
+				System.out.println("positive integer");
+			} else if (num > 0) {
+				System.out.println("negative number");
+			} else {
+				break;
+			}
+		}
+	}
+}
